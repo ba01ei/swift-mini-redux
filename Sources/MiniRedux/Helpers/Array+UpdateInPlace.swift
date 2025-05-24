2424cdd30042837d39eeb9fc3d0a7fca2ebdc85a
Bao Lei <bao@lei.fyi>
2025-05-24 14:49:30 -0700

    more powerful updateInPlace function to allow updating a segment

--- Sources/MiniRedux/Helpers/Array+UpdateInPlace.swift
+++ Sources/MiniRedux/Helpers/Array+UpdateInPlace.swift
@@ -3,49 +3,73 @@ extension Array where Element: Identifiable {
   /// Update a list in place, re-use existing items as much as possible
   /// This is useful for refreshing a list of stores based on new states fetched.
   ///  - parameter create: the closure to create new elements for the array, called only when necessary.
+  ///  - parameter filter: only update the items that passes the check. This allows the scenario where newItems should only cover a subset of the original list
   public mutating func updateInPlace<NewItem: Identifiable>(
     newItems: [NewItem],
-    create: (_ index: Int, NewItem) -> Element)
+    filter: (Element) -> Bool = { _ in return true },
+    create: (_ index: Int, _ newItem: NewItem) -> Element)
   where Element.ID == NewItem.ID {
-    var index = 0
+    updateInPlace(newItems: newItems, newItemId: \.id, filter: filter, create: create)
+  }
+  
+  public mutating func updateInPlace<NewItem>(
+    newItems: [NewItem],
+    newItemId: (NewItem) -> Element.ID,
+    filter: (Element) -> Bool = { _ in return true },
+    create: (_ index: Int, _ newItem: NewItem) -> Element
+  ) {
+    var originalArrayIndex = 0
+    var newArrayIndex = 0
     let originalIdSet = Set(map { $0.id })
-    while index < count && index < newItems.count {
-      let newItem = newItems[index]
-      if self[index].id == newItem.id {
-        index += 1
+    while originalArrayIndex < count && newArrayIndex < newItems.count {
+      if !filter(self[originalArrayIndex]) {
+        originalArrayIndex += 1
         continue
       }
-      if originalIdSet.contains(newItem.id),
-         let i = self.firstIndex(where: { $0.id == newItem.id}) {
+      
+      let newItem = newItems[newArrayIndex]
+      let newId = newItemId(newItem)
+      if self[originalArrayIndex].id == newId {
+        originalArrayIndex += 1
+        newArrayIndex += 1
+        continue
+      }
+      if originalIdSet.contains(newId),
+         let i = self.firstIndex(where: { $0.id == newId}) {
         /// A A
         /// B C index=1
         /// C D
         /// E B
         let matchingItem = remove(at: i)
-        insert(matchingItem, at: index)
-        index += 1
+        insert(matchingItem, at: originalArrayIndex)
+        originalArrayIndex += 1
+        newArrayIndex += 1
         continue
       } else {
         /// A A
         /// C C
         /// B D  index=1
         /// E B
-        insert(create(index, newItems[index]), at: index)
-        index += 1
+        insert(create(originalArrayIndex, newItems[newArrayIndex]), at: originalArrayIndex)
+        originalArrayIndex += 1
+        newArrayIndex += 1
         continue
       }
     }
     /// A A
     /// B
-    if index < count {
-      removeSubrange(index ..< count)
+    for i in (originalArrayIndex..<count).reversed() {
+      if filter(self[i]) {
+        remove(at: i)
+      }
     }
     /// ```
     /// A A
     ///   B
     /// ```
-    if index < newItems.count {
-      append(contentsOf: newItems[index ..< newItems.count].enumerated().map { create($0, $1) })
+    if newArrayIndex < newItems.count {
+      let insertIndex = (lastIndex(where: { filter($0) }) ?? count - 1) + 1
+      insert(contentsOf: newItems[newArrayIndex ..< newItems.count].enumerated().map { create($0 + insertIndex, $1) }, at: insertIndex)
     }
   }
 }
---
+++ Tests/MiniReduxTests/UpdateInPlace.swift
@@ -0,0 +1,94 @@
+//
+//  UpdateInPlace.swift
+//  swift-mini-redux
+//
+//  Created by Bao Lei on 5/24/25.
+//
+
+import MiniRedux
+import Testing
+
+@Test func updateInPlace() async throws {
+  
+  struct Item: Identifiable, Equatable {
+    let id: String
+    let value: Int
+  }
+  
+  var array = [Item(id: "a", value: 1), Item(id: "b", value: 2), Item(id: "d", value: 4)]
+  array.updateInPlace(newItems: ["b", "c", "a"], newItemId: \.self) { index, newId in
+    Item(id: newId, value: index)
+  }
+  #expect(array == [Item(id: "b", value: 2), Item(id: "c", value: 1), Item(id: "a", value: 1)])
+}
+
+@Test func updateInPlaceWithFilter() async throws {
+  
+  struct Item: Identifiable, Equatable {
+    let section: Int
+    let id: String
+    let value: Int
+  }
+  
+  var array = [
+    Item(section: 0, id: "a", value: 1),
+    Item(section: 0, id: "b", value: 2),
+    Item(section: 0, id: "c", value: 3),
+    Item(section: 1, id: "d", value: 4),
+    Item(section: 1, id: "e", value: 5),
+    Item(section: 1, id: "f", value: 6),
+    Item(section: 2, id: "g", value: 7),
+    Item(section: 2, id: "h", value: 8),
+    Item(section: 2, id: "i", value: 9)
+  ]
+
+  // update items in section 1
+  array.updateInPlace(newItems: ["d", "e", "ff"], newItemId: \.self, filter: { $0.section == 1}) { index, newId in
+    Item(section: 1, id: newId, value: index)
+  }
+  #expect(array == [
+    Item(section: 0, id: "a", value: 1),
+    Item(section: 0, id: "b", value: 2),
+    Item(section: 0, id: "c", value: 3),
+    Item(section: 1, id: "d", value: 4),
+    Item(section: 1, id: "e", value: 5),
+    Item(section: 1, id: "ff", value: 5),
+    Item(section: 2, id: "g", value: 7),
+    Item(section: 2, id: "h", value: 8),
+    Item(section: 2, id: "i", value: 9)
+  ])
+  
+  // add more items to section 0
+  array.updateInPlace(newItems: ["b", "a", "c", "cc", "ccc"], newItemId: \.self, filter: { $0.section == 0}) { index, newId in
+    Item(section: 0, id: newId, value: index)
+  }
+  #expect(array == [
+    Item(section: 0, id: "b", value: 2),
+    Item(section: 0, id: "a", value: 1),
+    Item(section: 0, id: "c", value: 3),
+    Item(section: 0, id: "cc", value: 3),
+    Item(section: 0, id: "ccc", value: 4),
+    Item(section: 1, id: "d", value: 4),
+    Item(section: 1, id: "e", value: 5),
+    Item(section: 1, id: "ff", value: 5),
+    Item(section: 2, id: "g", value: 7),
+    Item(section: 2, id: "h", value: 8),
+    Item(section: 2, id: "i", value: 9)
+  ])
+  
+  // remove items from section 2
+  array.updateInPlace(newItems: ["i"], newItemId: \.self, filter: { $0.section == 2}) { index, newId in
+    Item(section: 2, id: newId, value: index)
+  }
+  #expect(array == [
+    Item(section: 0, id: "b", value: 2),
+    Item(section: 0, id: "a", value: 1),
+    Item(section: 0, id: "c", value: 3),
+    Item(section: 0, id: "cc", value: 3),
+    Item(section: 0, id: "ccc", value: 4),
+    Item(section: 1, id: "d", value: 4),
+    Item(section: 1, id: "e", value: 5),
+    Item(section: 1, id: "ff", value: 5),
+    Item(section: 2, id: "i", value: 9)
+  ])
+}
