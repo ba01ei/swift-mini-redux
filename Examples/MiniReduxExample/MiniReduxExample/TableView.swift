1c290c7ee08e2ea06030ec129324141539d057e9
Bao Lei <bao@lei.fyi>
2026-01-19 16:48:35 -0800

    Observe store in UIView

--- Examples/MiniReduxExample/MiniReduxExample/TableView.swift
+++ Examples/MiniReduxExample/MiniReduxExample/TableView.swift
@@ -23,23 +23,44 @@ struct TableView: View {
   }
 }
 
+// Custom UITableView subclass for iOS 26+ automatic observation
+class TableViewWithObservation: UITableView {
+  var store: TableStore?
+
+  @available(iOS 26, *)
+  override func updateProperties() {
+    super.updateProperties()
+    if let store {
+      _ = store.content
+      // iOS 26 automatically tracks and updates
+      self.reloadData()
+    }
+  }
+}
+
 struct TableViewRepresentable: UIViewRepresentable {
 
   let store: TableStore
   
   func makeUIView(context: Context) -> UITableView {
-    let tableView = UITableView()
+    let tableView = TableViewWithObservation()
     tableView.dataSource = context.coordinator
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 44
     context.coordinator.tableView = tableView
-    context.coordinator.observeStore()
+    if #available(iOS 26, *) {
+      tableView.store = store
+    } else {
+      context.coordinator.observeStore()
+    }
     return tableView
   }
 
   func updateUIView(_ uiView: UITableView, context: Context) {
-    uiView.reloadData()
+    if #unavailable(iOS 26) {
+      uiView.reloadData()
+    }
   }
 
   func makeCoordinator() -> Coordinator {
@@ -54,16 +75,19 @@ struct TableViewRepresentable: UIViewRepresentable {
       self.store = store
     }
 
+    // backward compatibility for below iOS 26
     func observeStore() {
-      withObservationTracking {
-        // Access the store property to register observation
-        _ = store.content
-      } onChange: {
-        // When content changes, reload the table on main thread
-        Task { @MainActor in
-          self.tableView?.reloadData()
-          // Re-establish observation for next change
-          self.observeStore()
+      if #unavailable(iOS 26) {
+        withObservationTracking {
+          // Access the store property to register observation
+          _ = store.content
+        } onChange: {
+          // When content changes, reload the table on main thread
+          Task { @MainActor in
+            self.tableView?.reloadData()
+            // Re-establish observation for next change
+            self.observeStore()
+          }
         }
       }
     }
