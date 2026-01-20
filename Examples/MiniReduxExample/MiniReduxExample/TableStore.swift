5a36d7baa9ef8186fe229a73152cdc1d7bb3bb97
Bao Lei <bao@lei.fyi>
2026-01-19 16:31:04 -0800

    load quotes in tableview

--- Examples/MiniReduxExample/MiniReduxExample/TableStore.swift
+++ Examples/MiniReduxExample/MiniReduxExample/TableStore.swift
@@ -15,18 +15,37 @@ class TableStore: BaseStore<TableStore.Action>, Identifiable {
   // MARK: - State
   let id = UUID()
   var content: [String] = []
+  var isLoading = false
   
   // MARK: - Action
   enum Action {
     case addTapped
+    case quoteFetched(String)
   }
-  
+
   // MARK: - Reducer
   override func reduce(_ action: Action) -> Effect<Action> {
     switch action {
     case .addTapped:
-      return .none
+      isLoading = true
+      return .run { send in
+        guard let url = URL(string: "https://cipher.lei.fyi/quote") else {
+          await send(.quoteFetched(""))
+          return
+        }
+        do {
+          let response = try await URLSession.shared.data(for: URLRequest(url: url))
+          let quote = try JSONDecoder().decode(Quote.self, from: response.0)
+          await send(.quoteFetched("\(quote.quote) - \(quote.author)"))
+        } catch {
+          await send(.quoteFetched("We failed to fetch a quote because \(error). - This App"))
+        }
+      }
 
+    case .quoteFetched(let quote):
+      isLoading = false
+      content.append(quote)
+      return .none
     }
   }
 }
--- Examples/MiniReduxExample/MiniReduxExample/TableView.swift
+++ Examples/MiniReduxExample/MiniReduxExample/TableView.swift
@@ -1,33 +1,82 @@
 import SwiftUI
 import UIKit
+import MiniRedux
 
-struct TableView: UIViewRepresentable {
-  
+struct TableView: View {
+  let store: TableStore
+
+  var body: some View {
+    VStack {
+      HStack {
+        if store.isLoading {
+          ProgressView()
+            .padding()
+        }
+        Spacer()
+        Button("Add") {
+          store.send(.addTapped)
+        }
+        .padding()
+      }
+      TableViewRepresentable(store: store)
+    }
+  }
+}
+
+struct TableViewRepresentable: UIViewRepresentable {
+
   let store: TableStore
   
   func makeUIView(context: Context) -> UITableView {
     let tableView = UITableView()
     tableView.dataSource = context.coordinator
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 44
+    context.coordinator.tableView = tableView
+    context.coordinator.observeStore()
     return tableView
   }
-  
+
   func updateUIView(_ uiView: UITableView, context: Context) {
-    // No updates needed for static content
+    uiView.reloadData()
   }
-  
+
   func makeCoordinator() -> Coordinator {
-    Coordinator()
+    Coordinator(store: store)
   }
-  
+
   class Coordinator: NSObject, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 3
+    let store: TableStore
+    weak var tableView: UITableView?
+
+    init(store: TableStore) {
+      self.store = store
     }
-    
+
+    func observeStore() {
+      withObservationTracking {
+        // Access the store property to register observation
+        _ = store.content
+      } onChange: {
+        // When content changes, reload the table on main thread
+        Task { @MainActor in
+          self.tableView?.reloadData()
+          // Re-establish observation for next change
+          self.observeStore()
+        }
+      }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return store.content.count
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-      cell.textLabel?.text = "\(indexPath.row + 1)"
+      cell.textLabel?.text = store.content[indexPath.row]
+      cell.textLabel?.numberOfLines = 0
+      cell.textLabel?.lineBreakMode = .byWordWrapping
       return cell
     }
   }
